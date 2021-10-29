44f432b476b072bb3fc322e5c0be94b13efe9367
Pailla Raghavendra Reddy <raghavendra18042002@gmail.com>
2021-10-29 13:34:33 +0530

    Server Error

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv').config();
 
 const {createServer} = require('http');
 const next = require('next');
+const port = process.env.PORT || 3000;
 
 const  app = next({
     dev: process.env.NODE_ENV !== 'production'
@@ -12,9 +13,9 @@ const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-  createServer(handler).listen(3000,(err)=>{
+  createServer(handler).listen(port,(err)=>{
       if(err)throw err;
-      console.log('Ready on localhost:3000');
+      console.log('Server is up on port ' + port);
   });
 });
 
